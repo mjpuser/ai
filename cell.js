03321010dc2e91577132c2f8d42953c6facaa6e7
Matthew Prestifilippo <m.prestifilippo@gmail.com>
2016-05-31 20:42:45 -0400

    fix indentation

--- cell.js
+++ cell.js
@@ -49,75 +49,65 @@ const through2 = require('through2');
 const REFRACTOR_MULTIPLIER = 2;
 
 /**
- * Create a cell
- * @param {Number} state - initial state of the cell.
- * @param {Number} threshold - threshold needed for the cell to hit to create an impulse
- * @param {Number} trasmitter - the value transmitted when the impulse is triggered
- */
+* Create a cell
+* @param {Number} state - initial state of the cell.
+* @param {Number} threshold - threshold needed for the cell to hit to create an impulse
+* @param {Number} trasmitter - the value transmitted when the impulse is triggered
+*/
 module.exports = ({state, threshold, transmitter}) => {
-  state = state || 0;
-  threshold = threshold || 10;
-  transmitter = transmitter || 1;
-  let refractory = false;
-  // impulses are constantly being restored to 0 over time.  X number of
-  // impulses have to come in around the same time.
+   state = state || 0;
+   threshold = threshold || 10;
+   transmitter = transmitter || 1;
+   let refractory = false;
+   // impulses are constantly being restored to 0 over time.  X number of
+   // impulses have to come in around the same time.
 
-  // during action potential, there is a bit of time where the neuron
-  // doesn't accept new inputs.
+   // during action potential, there is a bit of time where the neuron
+   // doesn't accept new inputs called the refractory period
 
-  // one way we can represent this is with a queue that has a count.  the
-  // neuron is always recieving input, and those values are either -1, 0, or 1.
-  // When the value of the queue reaches 10, then it is gonna trigger it's
-  // action potential.  The queue should have a fixed size of say 40.  The
-  // downside of this design is that it always has to be doing something
-  // instead of being "silent" and waiting for events.  How could we
-  // describe this phenomenon without that?  I don't know if there is one.
-  // we can always do a nextTick to neutralize the action potential.  The
-  // idea is that there won't be nearly as many since the inputs are sparse.
-
-  const neutralize = () => {
-    if (state) {
+   const neutralize = () => {
+      if (state) {
+         process.nextTick(() => {
+            state = parseInt(state / REFRACTOR_MULTIPLIER);
+            if (state) {
+               neutralize();
+            }
+            else {
+               refractory = false;
+            }
+         });
+      }
+   };
+   const sum = (input) => {
+      state = input.reduce((sum, val) => {
+         return sum + val;
+      }, state);
+   };
+   const impulse = (cell) => {
       process.nextTick(() => {
-        state = parseInt(state / REFRACTOR_MULTIPLIER);
-        if (state) {
-          neutralize();
-        }
-        else {
-          refractory = false;
-        }
+         state = threshold * 3;
+         refractory = true;
+         cell.push(transmitter);
+         neutralize();
       });
-    }
-  };
-  const sum = (input) => {
-    state = input.reduce((sum, val) => {
-      return sum + val;
-    }, state);
-  };
-  const impulse = (cell) => {
-    process.nextTick(() => {
-      state = threshold * 3;
-      refractory = true;
-      cell.push(transmitter);
-      neutralize();
-    });
-  };
+   };
 
 
-  return through2({
-    objectMode: true
-  },
-  function (input, encoding, callback) {
-    //console.log('state', state, 'refractory', refractory, 'multiplier', REFRACTOR_MULTIPLIER);
-    if (refractory) {
-      return callback();
-    }
-    sum(input);
-    if (state >= threshold) {
-      impulse(this);
-    }
-    else {
-      neutralize();
-    }
-    callback();
-  });
+   return through2({
+      objectMode: true
+   },
+   function (input, encoding, callback) {
+      //console.log('state', state, 'refractory', refractory, 'multiplier', REFRACTOR_MULTIPLIER);
+      if (refractory) {
+         return callback();
+      }
+      sum(input);
+      if (state >= threshold) {
+         impulse(this);
+      }
+      else {
+         neutralize();
+      }
+      callback();
+   });
 };
