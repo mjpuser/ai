8e86c547af83fda4069c5cf201d86834619f0804
Matthew Prestifilippo <mattp@aweber.com>
2019-01-17 06:31:22 -0500

    Modifiactions to server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const retina = require('./app');
-//
-const spawn = require('child_process').spawn;
-
-const ffmpeg = spawn('ffmpeg',
-   `-loglevel panic -video_size ${retina.dimension}x${retina.dimension} -framerate 1 -f x11grab -i :0.0+100,100 -c:v rawvideo -pix_fmt:v rgb24 -f rawvideo -`.split(' ')
+const spawn = require('child_process').exec;
+console.log(
+   `-loglevel panic -video_size ${retina.dimension}x${retina.dimension} -framerate 1 -f x11grab -i :0.0+100,100 -c:v rawvideo -pix_fmt:v rgb24 -f rawvideo -`
+);
+console.log(
+   `ffmpeg -loglevel panic -video_size ${retina.dimension}x${retina.dimension} -framerate 1 -f avfoundation -i "1" -c:v rawvideo -pix_fmt:v rgb24 -f rawvideo -`
+);
+const ffmpeg = spawn('/usr/local/bin/ffmpeg ' +
+   `-loglevel panic -video_size ${retina.dimension}x${retina.dimension} -framerate 1 -f avfoundation -i "1" -c:v rawvideo -pix_fmt:v rgb24 -f rawvideo -`
 );
 
 const colors = ['r', 'g', 'b'];
@@ -14,10 +18,9 @@ const inputTo = (color, rgb, index) => {
    if (cola === 'r') {
       try {
          console.log('rgb', rgb.readUInt8(0));
-         retina[cola][index].write(rgb);
+         // retina[cola][index].write(rgb);
       } catch(e) {
          console.log('whaup?', rgb);
-         // swallow?
       }
    }
 };
@@ -28,25 +31,28 @@ const printOutput = (cs, i) => {
    });
 };
 
-ffmpeg.stdout.on('data', (chunk) => {
-   const entries = chunk.entries();
-   let count = 0;
-   for (let entry of entries) {
-      const rgbVal = entry[1];
-      inputTo(count % 3, Buffer.from(rgbVal.toString(16), 'hex'), Math.floor(count / 3), retina);
-      count++;
-      if (count === retina.total) {
-         count = 0;
-      }
-   }
+let buffer = Buffer.from([]);
+ffmpeg.stdout.on('data', chunk => {
+    buffer = Buffer.concat([ buffer, Buffer.from(chunk) ]);
+    const entries = buffer.entries();
+    let count = 0;
+    for (let entry of entries) {
+       const rgbVal = entry[1];
+       inputTo(count % 3, Buffer.from(rgbVal.toString(16), 'hex'), Math.floor(count / 3), retina);
+       count++;
+       if (count === retina.total) {
+          count = 0;
+       }
+    }
 });
-retina.monster.r.forEach(printOutput);
+// retina.monster.r.forEach(printOutput);
 
-ffmpeg.stdout.on('close', (code) => {
-   console.log(`this closed ${code}`);
+ffmpeg.stdout.on('close', code => {
+    console.log(`this closed ${code}`);
 });
-
-//
+ffmpeg.on('exit', (err, errr) => {
+    console.error(err);
+});
 // process.stdin.on('data', (chunk) => {
 //    console.log('data');
 // });
